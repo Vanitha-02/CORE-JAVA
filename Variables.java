7b2e378ea2372cc6d6edc630021d0fe30ae01b82
Vanitha-02 <vanithagvt02@gmail.com>
2024-11-05 11:39:02 +0530

    variables file

---
+++ Variables.java
@@ -0,0 +1,76 @@
+public class Variables{
+	public static void main(String[] args) {
+
+
+		int rank=30;
+		System.out.println(rank);
+		rank=50;
+		System.out.println(rank);
+
+		int score=80;
+		System.out.println(score);
+		score=90;
+		System.out.println(score);
+
+		int book_price=50;
+		System.out.println(book_price);
+		book_price=100;
+		System.out.println(book_price);
+
+	    long phone_no=9876543212l;
+		System.out.println(phone_no);
+
+		int book_id=312;
+		System.out.println(book_id);
+
+		int password=24670;
+		System.out.println(password);
+
+		int police=103;
+		System.out.println(police);
+
+		int block_no=456;
+		System.out.println(block_no);
+
+		int roll_no=12;
+		System.out.println(roll_no);
+
+		int helpline_no=104;
+		System.out.println(helpline_no);
+
+		int ambulance=100;
+		System.out.println(ambulance);
+
+		int gift=3;
+		System.out.println(gift);
+
+		int emergency_no=100;
+		System.out.println(emergency_no);
+
+		long adhar_no=4221643221234l;
+		System.out.println(adhar_no);
+
+		long pan_no=3216778889l;
+		System.out.println(pan_no);
+
+		int year=2003;
+		System.out.println(year);
+
+		int weight=40;
+		System.out.println(weight);
+
+		int height=5;
+		System.out.println(height);
+
+		int ticket=6;
+		System.out.println(ticket);
+
+
+
+
+
+
+
+		
+	}
+}
