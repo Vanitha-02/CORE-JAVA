6ccce07432f6b898cc075f0bd236298454cf2cc0
Vanitha-02 <vanithagvt02@gmail.com>
2024-11-06 20:10:12 +0530

    type od data

---
+++ Datatypes.java
@@ -0,0 +1,141 @@
+public class Datatypes{
+	public static void main(String[] args){
+
+	 byte age=100;
+	 System.out.println(age);
+	 byte percentage=80;
+	 System.out.println(percentage);
+	 byte classInSchool=2;
+	 System.out.println(classInSchool);
+	 byte month=12;
+	 System.out.println(month);
+	 byte daysInMonth=30;
+	 System.out.println(daysInMonth);
+	 byte statesInIndia=29;
+	 System.out.println(statesInIndia);
+     byte schoolBag=1;
+     System.out.println(schoolBag);
+     byte bookPerSubject=2;
+     System.out.println(bookPerSubject);
+     byte applePerDay=1;
+     System.out.println(applePerDay);
+     byte topperInClass=2;
+     System.out.println(topperInClass);
+
+
+     short daysInYear=365;
+     System.out.println(daysInYear);
+     short membersOfVidhanaSabha=500;
+     System.out.println(membersOfVidhanaSabha);
+     short studentsInCollege=4000;
+     System.out.println(studentsInCollege);
+     short riversInIndia=400;
+     System.out.println(riversInIndia); 
+     short childline_no=1098;
+     System.out.println(childline_no);
+     short countriesInIndia=195;
+     System.out.println(countriesInIndia);
+     short examFeeInCollege=3000;
+     System.out.println(examFeeInCollege);
+     short numberPlate=1036;
+     System.out.println(numberPlate);
+
+
+     int phone_no=34566;
+     System.out.println(phone_no);
+     int students=50000;
+     System.out.println(students);
+     int animals=91000;
+     System.out.println(animals);
+     int books=200000;
+     System.out.println(books);
+     int price=100000;
+     System.out.println(price);
+     int population=149000;
+     System.out.println(population);
+     int minutes=21900;
+     System.out.println(minutes);
+     int rural=49000;
+     System.out.println(rural);
+
+
+     float weight=3.2f;
+     System.out.println(weight);
+     float height=6.5f;
+     System.out.println(height);
+     float cgpa=8.5f;
+     System.out.println(cgpa);
+     float concentration=0.01f;
+     System.out.println(concentration);
+     float waterLevel=45.6f;
+     System.out.println(waterLevel);
+     float velocity=56.32f;
+     System.out.println(velocity);
+
+     long peoples=234567890l;
+     System.out.println(population);
+
+     long adhar_number=123456789876l;
+     System.out.println(adhar_number);
+     long carecentre_no=2345678999l;
+     System.out.println(phone_no);
+     long tollfree_no=234567892l;
+     System.out.println(tollfree_no);
+     long emergency_no=9087465313l;
+     System.out.println(emergency_no);
+     long pan_no=978634625l;
+     System.out.println(pan_no);
+     long mobile_no=989675456l;
+     System.out.println(mobile_no);
+     long salary=150000000l;
+     System.out.println(salary);
+
+
+     double gravity=234.14567d;
+     System.out.println(gravity);
+     double distance=35.67d;
+     System.out.println(distance);
+     double radius=10.2d;
+     System.out.println(radius);
+     double area=95.03317d;
+     System.out.println(area);
+     double average=15.345d;
+     System.out.println(average);
+     double turbidity=0.567d;
+     System.out.println(turbidity);
+     double water_content=78.876d;
+     System.out.println(water_content);
+     double hardness=56.6356d;
+     System.out.println(hardness);
+
+
+     boolean isItCorrect=true;
+     System.out.println(isItCorrect);
+     boolean isFlowerBeautiful=true;
+     System.out.println(isFlowerBeautiful);
+     boolean isItRaining=true;
+     System.out.println(isItRaining);
+     boolean isItClassic=false;
+     System.out.println(isItClassic);
+     boolean isMail=true;
+     System.out.println(isMail);
+     boolean isItComplex=false;
+     System.out.println(isItComplex);
+     boolean isAnOverWeight=true;
+     System.out.println(isAnOverWeight);
+
+
+     char letter='a';
+     System.out.println(letter);
+     char option='b';
+     System.out.println(option);
+     char male='M';
+     System.out.println(male);
+     char female='F';
+     System.out.println(female);
+     char alphabet='h';
+     System.out.println(alphabet);
+     char section='D';
+     System.out.println(section);
+ }
+}
