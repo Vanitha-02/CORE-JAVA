57356c7ce0f7752d5e009b01fa253c8ca98c5e55
Vanitha-02 <vanithagvt02@gmail.com>
2024-11-07 20:10:40 +0530

    Example method

---
+++ Method.java
@@ -0,0 +1,23 @@
+public class Method{
+	 public static void main(String[] args){
+	 System.out.println("Hello World");
+	 System.out.println("I am learning java");
+	 System.out.println("It is awesome");	
+	 sub();
+     sub();
+     mul();
+     mul();
+     }
+
+	  public static void sub(){
+	   int a=10;
+	   int b=20;
+	  System.out.println(a-b);
+	  }
+
+	  public static void mul(){
+	  	int a=20;
+	  	int b=15;
+	  System.out.println(a*b);	
+	  }
+}
