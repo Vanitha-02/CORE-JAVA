24f89d3cb2d77d3460ea2197080b6c57f7e3fab5
Vanitha-02 <vanithagvt02@gmail.com>
2024-11-12 20:09:25 +0530

    example of array

---
+++ Arrays.java
@@ -0,0 +1,138 @@
+public class Arrays{
+	public static void main(String[] args){
+
+	int[] items={10,23,45,56,78,20,43,27,42,54};
+	System.out.println(items[0]);
+	System.out.println(items[1]);
+	System.out.println(items[2]);
+	System.out.println(items[3]);
+	System.out.println(items[4]);
+	System.out.println(items[5]);
+	System.out.println(items[6]);
+	System.out.println(items[7]);
+	System.out.println(items[8]);
+	System.out.println(items[9]);
+    
+
+    items[1]=25;
+    System.out.println(items[1]);
+    items[4]=80;
+    System.out.println(items[4]);
+    items[6]=45;
+    System.out.println(items[6]);
+
+
+    int[] item=new int[10];
+	item[0]=1;
+	System.out.println(item[0]);
+	item[1]=2;
+	System.out.println(item[1]);
+	item[2]=3;
+	System.out.println(item[2]);
+	item[3]=4;
+	System.out.println(item[3]);
+	item[4]=5;
+	System.out.println(item[4]);
+	item[5]=6;
+	System.out.println(item[5]);
+	item[6]=7;
+	System.out.println(item[6]);
+	item[7]=8;
+	System.out.println(item[7]);
+	item[8]=9;
+	System.out.println(item[8]);
+	item[9]=10;
+	System.out.println(item[9]);
+
+
+
+
+	byte[] cards={2,6,9,30,37,38,89,57,38,29};
+	System.out.println(cards[0]);
+	System.out.println(cards[1]);
+	System.out.println(cards[2]);
+	System.out.println(cards[3]);
+	System.out.println(cards[4]);
+	System.out.println(cards[5]);
+	System.out.println(cards[6]);
+	System.out.println(cards[7]);
+	System.out.println(cards[8]);
+	System.out.println(cards[9]);
+
+
+	cards[2]=12;
+	System.out.println(cards[2]);
+	cards[4]=40;
+	System.out.println(cards[4]);
+	cards[6]=93;
+	System.out.println(cards[6]);
+	
+
+	byte[] card=new byte[10];
+	card[0]=10;
+	System.out.println(card[0]);
+	card[1]=20;
+	System.out.println(card[1]);
+    card[2]=30;
+    System.out.println(card[2]);
+    card[3]=40;
+    System.out.println(card[3]);
+    card[4]=50;
+    System.out.println(card[4]);
+    card[5]=60;
+    System.out.println(card[5]);
+    card[6]=70;
+    System.out.println(card[6]);
+    card[7]=80;
+    System.out.println(card[7]);
+    card[8]=90;
+    System.out.println(card[8]);
+	card[9]=100;
+    System.out.println(card[9]);
+
+
+    short[] members={230,100,234,23,34,78,79,678,8976,345};
+    System.out.println(members[0]);
+	System.out.println(members[1]);
+	System.out.println(members[2]);
+	System.out.println(members[3]);
+	System.out.println(members[4]);
+	System.out.println(members[5]);
+	System.out.println(members[6]);
+	System.out.println(members[7]);
+	System.out.println(members[8]);
+	System.out.println(members[9]);
+	
+    
+    members[2]=235;
+    System.out.println(members[2]);
+	members[3]=26;
+	System.out.println(members[3]);
+	members[4]=36;
+	System.out.println(members[4]);
+
+
+     	
+   	
+
+   	
+
+
+	 	
+ 
+  
+	
+	
+	
+	
+
+ 	
+	
+	
+	
+	
+	
+	
+	
+	}
+}
