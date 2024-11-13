91c973cc62df5415cfcf0d7bcf3a6a43afa6c2b8
Vanitha-02 <vanithagvt02@gmail.com>
2024-11-13 19:41:23 +0530

    Array Example

--- Arrays.java
+++ Arrays.java
@@ -91,7 +91,7 @@ public class Arrays{
     System.out.println(card[9]);
 
 
-    short[] members={230,100,234,23,34,78,79,678,8976,345};
+    short[] members={230,100,234,23,34,78,79,678,89,76,345};
     System.out.println(members[0]);
 	System.out.println(members[1]);
 	System.out.println(members[2]);
@@ -112,27 +112,249 @@ public class Arrays{
 	System.out.println(members[4]);
 
 
-     	
+    short[] member=new short[10];
+    member[0]=23;
+    System.out.println(member[0]);
+    member[1]=34;
+    System.out.println(member[1]);
+    member[2]=65;
+    System.out.println(member[2]);
+    member[3]=35;
+    System.out.println(member[3]);
+    member[4]=44;
+    System.out.println(member[4]);
+    member[5]=26;
+    System.out.println(member[5]);
+    member[6]=66;
+    System.out.println(member[6]);
+    member[7]=76;
+    System.out.println(member[7]);
+    member[8]=43;
+    System.out.println(member[8]);
+    member[9]=32;
+    System.out.println(member[9]); 
+
+
+    float[] height={3.5f,5.6f,6.4f,6.8f,6.9f,3.6f,5.4f,2.8f,4.9f,5.1f};
+    System.out.println(height[0]);
+    System.out.println(height[1]);
+    System.out.println(height[2]); 
+    System.out.println(height[3]); 
+    System.out.println(height[4]); 
+    System.out.println(height[5]); 
+    System.out.println(height[6]); 
+    System.out.println(height[7]); 
+    System.out.println(height[8]); 
+    System.out.println(height[9]); 
+    
+
+    height[1]=0.3f;
+    System.out.println(height[1]); 
+    height[3]=6.7f;
+    System.out.println(height[3]); 
+    height[5]=8.6f;
+    System.out.println(height[5]); 
+
+    float[] heights=new float[10];
+    heights[0]=0.3f;
+    System.out.println(heights[0]);
+    heights[1]=4.3f;
+    System.out.println(heights[1]);
+    heights[2]=5.3f;
+    System.out.println(heights[2]);
+    heights[3]=6.3f;
+    System.out.println(heights[3]);
+    heights[4]=7.3f;
+    System.out.println(heights[4]);
+    heights[5]=8.3f;
+    System.out.println(heights[5]);
+    heights[6]=9.3f;
+    System.out.println(heights[6]);
+    heights[7]=1.3f;
+    System.out.println(heights[7]);
+    heights[8]=2.3f;
+    System.out.println(heights[8]);
+    heights[9]=3.3f;
+    System.out.println(heights[9]);
+
+
+
+
+    double[] distances={23.4d,34.5d,56.76d,45.35d,57.98d,52.03d,32.97d,98.456d,43.567d,87.432d};
+    System.out.println(distances[0]);
+    System.out.println(distances[1]);
+    System.out.println(distances[1]);
+    System.out.println(distances[2]);
+    System.out.println(distances[3]);
+    System.out.println(distances[4]);
+    System.out.println(distances[5]);
+    System.out.println(distances[6]);
+    System.out.println(distances[7]);
+    System.out.println(distances[8]);
+    System.out.println(distances[9]);
+    
+
+    distances[1]=23.87d;
+    System.out.println(distances[1]);
+    distances[5]=67.09d;
+    System.out.println(distances[5]);
+    distances[9]=67.786d;
+    System.out.println(distances[9]);
+
+
+    double[] distance=new double[10];
+    distance[0]=24.2d;
+    System.out.println(distance[0]);
+    distance[1]=24.2d;
+    System.out.println(distance[1]);
+    distance[2]=24.2d;
+    System.out.println(distance[2]);
+    distance[3]=24.2d;
+    System.out.println(distance[3]);
+    distance[4]=24.2d;
+    System.out.println(distance[4]);
+    distance[5]=44.3d;
+    System.out.println(distance[5]);
+    distance[6]=32.2d;
+    System.out.println(distance[6]);
+    distance[7]=54.22d;
+    System.out.println(distance[7]);
+    distance[8]=76.28d;
+    System.out.println(distance[8]);
+    distance[9]=26.12d;
+    System.out.println(distance[9]);
+
+
+
+    long[] mobile_no={2334455l,546373882l,5463738l,876543468l,54672818l,64783829l,5464782l,987654l,1234567l,9036435l};
+    System.out.println(mobile_no[0]);
+    System.out.println(mobile_no[1]);
+    System.out.println(mobile_no[2]);
+    System.out.println(mobile_no[3]);
+    System.out.println(mobile_no[4]);
+    System.out.println(mobile_no[5]);
+    System.out.println(mobile_no[7]);
+    System.out.println(mobile_no[8]);
+    System.out.println(mobile_no[9]);
+    
+
+
+
+    mobile_no[3]=65773882l;
+    System.out.println(mobile_no[3]);
+    mobile_no[4]=65773832l;
+    System.out.println(mobile_no[4]);
+    mobile_no[5]=6564773838l;
+    System.out.println(mobile_no[5]);
+
+
+
+    long[] mobile_nos=new long[10];
+    mobile_nos[0]=45667899l;
+    System.out.println(mobile_nos[0]); 
+    mobile_nos[1]=45667899l;
+    System.out.println(mobile_nos[1]);
+    mobile_nos[2]=45667899l;
+    System.out.println(mobile_nos[2]);
+    mobile_nos[3]=45667899l;
+    System.out.println(mobile_nos[3]);
+    mobile_nos[4]=45667899l;
+    System.out.println(mobile_nos[4]);
+    mobile_nos[5]=45667899l;
+    System.out.println(mobile_nos[5]);
+    mobile_nos[6]=45667899l;
+    System.out.println(mobile_nos[6]);
+    mobile_nos[7]=45667899l;
+    System.out.println(mobile_nos[7]);
+    mobile_nos[8]=45667899l;
+    System.out.println(mobile_nos[8]);
+    mobile_nos[9]=45667899l;
+    System.out.println(mobile_nos[9]);
+
+
+
+    char[] alphabet={'A','B','C','D','E','F','G','H','I','J'};
+    System.out.println(alphabet[0]);
+    System.out.println(alphabet[1]);
+    System.out.println(alphabet[2]);
+    System.out.println(alphabet[3]);
+    System.out.println(alphabet[4]);
+    System.out.println(alphabet[5]);
+    System.out.println(alphabet[6]);
+    System.out.println(alphabet[7]);
+    System.out.println(alphabet[8]);
+    System.out.println(alphabet[9]);
+
+
+
+    alphabet[9]='m';
+    System.out.println(alphabet[9]); 
+    alphabet[8]='k';
+    System.out.println(alphabet[8]);
+    alphabet[7]='v';
+    System.out.println(alphabet[7]);
+
+
+    char[] alphabets=new char[10];
+    alphabets[0]='y';
+    System.out.println(alphabets[0]);
+    alphabets[1]='w';
+    System.out.println(alphabets[1]);
+    alphabets[2]='z';
+    System.out.println(alphabets[2]);
+    alphabets[3]='x';
+    System.out.println(alphabets[3]);
+    alphabets[4]='l';
+    System.out.println(alphabets[4]);
+    alphabets[5]='a';
+    System.out.println(alphabets[5]);
+    alphabets[6]='j';
+    System.out.println(alphabets[6]);
+    alphabets[7]='d';
+    System.out.println(alphabets[7]);
+    alphabets[8]='r';
+    System.out.println(alphabets[8]);
+    alphabets[9]='n';
+    System.out.println(alphabets[9]);
+
+   	boolean[] isOverWeight={true,false,true,false,true,true,false};
+   	System.out.println(isOverWeight[0]);
+   	System.out.println(isOverWeight[1]);
+   	System.out.println(isOverWeight[2]);
+   	System.out.println(isOverWeight[3]);
+   	System.out.println(isOverWeight[4]);
+   	System.out.println(isOverWeight[5]);
+   	System.out.println(isOverWeight[6]);
+   	
    	
 
+     boolean[] isFishTasty=new boolean[10];
+   	 isFishTasty[0]=true;
+   	 System.out.println(isFishTasty[0]);
+   	 isFishTasty[1]=false;
+   	 System.out.println(isFishTasty[1]);
+     isFishTasty[2]=true;
+	 System.out.println(isFishTasty[2]);
+   	 isFishTasty[3]=false;
+   	 System.out.println(isFishTasty[3]);
+     isFishTasty[4]=true;
+   	 System.out.println(isFishTasty[4]);
+     isFishTasty[5]=false;
+   	 System.out.println(isFishTasty[5]);
+   	 isFishTasty[6]=true;
+   	 System.out.println(isFishTasty[6]);
+   	 
+   	 
+
+   	 
+
+   		
+   	}}
+
    	
 
-
-	 	
- 
-  
 	
 	
 	
 	
 
- 	
-	
-	
-	
-	
-	
-	
-	
-	}
-}
